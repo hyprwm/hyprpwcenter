c2d57ac6dd1a2cb539e869b703403474e2e4f4c2
VESSE Léo <leovesse@gmail.com>
2025-10-27 21:09:16 +0100

    feat: flaking hyprpwcenter (#5)
    Co-authored-by: VESSE Leo <leo.vesse@pm.me>
    Co-authored-by: Mihai Fufezan <mihai@fufexan.net>

---
+++ nix/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  pkg-config,
+  aquamarine,
+  cairo,
+  hyprgraphics,
+  hyprtoolkit,
+  hyprutils,
+  pipewire,
+  libdrm,
+  pixman,
+  version ? "git",
+}:
+stdenv.mkDerivation {
+  pname = "hyprpwcenter";
+  inherit version;
+
+  src = ../.;
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
+
+  buildInputs = [
+    aquamarine
+    cairo
+    libdrm
+    hyprgraphics
+    hyprtoolkit
+    hyprutils
+    pipewire
+    pixman
+  ];
+
+  meta = {
+    homepage = "https://github.com/hyprwm/hyprpwcenter";
+    description = "A GUI Pipewire control center";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.linux;
+    mainProgram = "hyprpwcenter";
+  };
+}
